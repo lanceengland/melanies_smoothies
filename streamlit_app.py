7fb80db8c90e34d9e6647544cd755a3d3634b274
Lance England <lance.england@gmail.com>
2025-01-18 19:49:02 -0500

    Update streamlit_app.py
    hooked up api to furits selected

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,11 +36,12 @@ if ingredients_list:
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,NAME_ON_ORDER)
         values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
   
-    st.write(my_insert_stmt)
+    #st.write(my_insert_stmt)
+    st.subheader(item + ' Nutrition Information')
+    smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + item)
+    sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
     
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
